8acd54eba3e8a3d2fc4a772f9fbecc683c931318
William Harris <17HarriW@fulford.york.sch.uk>
2022-10-03 11:02:55 +0100

    Added iteration

--- Performance/Program.cs
+++ Performance/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,24 @@ namespace Performance
 {
     class Program
     {
+        static void Test()
+        {
+            List<int> myList = new List<int>();
+            for (int i = 0; i < 220000000; i++)
+            {
+                myList.Add(i);
+            }
+        }
         static void Main(string[] args)
         {
+            Stopwatch s = new Stopwatch();
+            s.Start();
+            for (int i = 0; i < 2; i++)
+                Test();
+            s.Stop();
+            Console.WriteLine($"{s.ElapsedMilliseconds}ms taken");
 
+            Console.ReadLine();
         }
     }
 }
